c3a94efa1af3d405a482c4cbd5906ab0fc39581a
vivek773 <vk5636040@gmail.com>
2022-04-20 18:45:35 +0530

    Finally done

--- js/app.js
+++ js/app.js
@@ -2,18 +2,18 @@ const toggleForm = () => {
   const container = document.querySelector(".container");
   container.classList.toggle("active");
 };
-// store data in local storage
+// store SignUpdata in local storage
 function Store() {
-  let data = localStorage.getItem("SignUpdata")
-    ? JSON.parse(localStorage.getItem("SignUpdata"))
+  let data = localStorage.getItem("Store")
+    ? JSON.parse(localStorage.getItem("Store"))
     : [];
   let formData = {
-    name: document.getElementById("uName").value,
-    email: document.getElementById("uEmail").value,
-    password: document.getElementById("uCreatePassword").value,
-    confirmpassword: document.getElementById("uConfirmPassword").value,
+    "name" : document.getElementById("uName").value,
+    "email": document.getElementById("uEmail").value,
+    "password": document.getElementById("uCreatePassword").value,
+    "confirmpassword": document.getElementById("uConfirmPassword").value,
   };
-  data.push(formData);
+  data.push(formData); 
   if (localStorage) {
     localStorage.setItem("Store", JSON.stringify(data));
   }
@@ -21,39 +21,44 @@ function Store() {
 // password matching
 function verifyPassword(input) {
   if (input.value != uCreatePassword.value) {
-    input.setCustomValidity("Password Must be Matching");
+    input.setCustomValidity("Password Must be Matching"); 
   } else {
     input.setCustomValidity("");
   }
 }
-//check already registered users
+// For UniQue (email)
 function AlreadyExists(value) {
-  let existemail = JSON.parse(localStorage.getItem("SignUpdata"));
-  let getexistemail = existemail.filter((emailData) => {
+  let existemail = JSON.parse(localStorage.getItem("Store"));
+  let getexistemail = existemail && existemail.filter((emailData) => {
     if (emailData.email == value.value) {
       value.setCustomValidity("email exist. try something else");
     } else {
       value.setCustomValidity("");
     }
-    console.log(getexistemail);
   });
+  console.log(getexistemail);
 }
-// Login 
-function login(){   
-    let loginEmail = document.getElementById("uemail").value;
-    let loginPass =  document.getElementById("uPassword").value;
-    let matchEmail = JSON.parse(localStorage.getItem("SignUpdata"));
-    console.log(matchEmail)
-    let emailArray =[];
-    let passArray=[];
-     let result = matchEmail.map((emailData) =>{
-       emailArray.push(emailData.email);
-       passArray.push(emailData.password);
-    });
-    if(emailArray.indexOf(loginEmail) > -1 && passArray.indexOf(loginPass) > -1){
-        alert("You have sucsessfuly loged in");
-    }else{
-        alert("You have no registered with us");
-    }
+// Login Portion
+function loginUser() {
+  let loginEmail = document.getElementById("uemail").value;
+  let loginPass = document.getElementById("uPassword").value;
+  let matchEmail = JSON.parse(localStorage.getItem("Store"));
+  console.log(matchEmail);
+  let emailArray = [];
+  let passArray = [];
+  let result = matchEmail && matchEmail.map((emailData) => {
+    emailArray.push(emailData.email);
+    passArray.push(emailData.password);
+  });
+  console.log(result);
+  if (
+    emailArray.indexOf(loginEmail) > -1 &&
+    passArray.indexOf(loginPass) > -1
+   ) {
+     alert("You have successfully loged in");
+   }  else {
+    alert("You have no registered with us");
+   }
+ }
   
-}
+ // vivek
